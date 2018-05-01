1091f6131b42458e27efd867723d09320de7b764
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-05-01 18:31:02 +0300

    Added auto-registration in monitoring service

---
+++ src/Lykke.Service.HftInternalService.Core/MonitoringServiceClientSettings.cs
@@ -0,0 +1,10 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.HftInternalService.Core
+{
+    public class MonitoringServiceClientSettings
+    {
+        [HttpCheck("api/isalive")]
+        public string MonitoringServiceUrl { get; set; }
+    }
+}
